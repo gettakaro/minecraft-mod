453c8cdb86c6c4abc825bf02a694d8c4d7c6df3c
Sean Stoves <sean@seanstoves.com>
2024-10-12 17:09:46 -0400

    Refactor WebSocket.java: Update startWebSocketServer method to accept hostname, port, and logger

--- src/main/java/com/takaro/takaroplugin/WebSocket.java
+++ src/main/java/com/takaro/takaroplugin/WebSocket.java
@@ -20,44 +20,41 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class WebSocket extends JavaPlugin {
     public static void startWebSocketServer(String host, int port, Logger logger) throws IOException, NoSuchAlgorithmException {
         ServerSocket server = new ServerSocket(port, 50, InetAddress.getByName(host));
-        try {
-            logger.info("Server has started on " + host + ":" + port + ".\r\nWaiting for a connection…");
-            Socket client = server.accept();
-            logger.info("A client connected.");
 
-            InputStream in = client.getInputStream();
-            OutputStream out = client.getOutputStream();
+        logger.info("Server has started on " + host + ":" + port + ".\r\nWaiting for a connection…");
+        Socket client = server.accept();
+        logger.info("A client connected.");
 
-            // Read the client's request
-            Scanner s = new Scanner(in, "UTF-8");
-            String data = s.useDelimiter("\\r\\n\\r\\n").next();
-            Matcher get = Pattern.compile("^GET").matcher(data);
-            if (get.find()) {
-                Matcher match = Pattern.compile("Sec-WebSocket-Key: (.*)").matcher(data);
-                match.find();
-                byte[] response = ("HTTP/1.1 101 Switching Protocols\r\n"
-                        + "Connection: Upgrade\r\n"
-                        + "Upgrade: websocket\r\n"
-                        + "Sec-WebSocket-Accept: "
-                        + Base64.getEncoder().encodeToString(
-                        MessageDigest.getInstance("SHA-1").digest(
-                                (match.group(1) + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11")
-                                        .getBytes("UTF-8")))
-                        + "\r\n\r\n")
-                        .getBytes("UTF-8");
+        InputStream in = client.getInputStream();
+        OutputStream out = client.getOutputStream();
 
-                out.write(response, 0, response.length);
-            }
+        // Read the client's request
+        Scanner s = new Scanner(in, "UTF-8");
+        String data = s.useDelimiter("\\r\\n\\r\\n").next();
+        Matcher get = Pattern.compile("^GET").matcher(data);
+        if (get.find()) {
+            Matcher match = Pattern.compile("Sec-WebSocket-Key: (.*)").matcher(data);
+            match.find();
+            byte[] response = ("HTTP/1.1 101 Switching Protocols\r\n"
+                    + "Connection: Upgrade\r\n"
+                    + "Upgrade: websocket\r\n"
+                    + "Sec-WebSocket-Accept: "
+                    + Base64.getEncoder().encodeToString(
+                    MessageDigest.getInstance("SHA-1").digest(
+                            (match.group(1) + "258EAFA5-E914-47DA-95CA-C5AB0DC85B11")
+                                    .getBytes("UTF-8")))
+                    + "\r\n\r\n")
+                    .getBytes("UTF-8");
 
-            // Now you can read/write data frames from/to the client
-            // This is a simple echo server example
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = in.read(buffer)) != -1) {
-                out.write(buffer, 0, bytesRead);
-            }
-        } finally {
-            server.close();
+            out.write(response, 0, response.length);
+        }
+
+        // Now you can read/write data frames from/to the client
+        // This is a simple echo server example
+        byte[] buffer = new byte[1024];
+        int bytesRead;
+        while ((bytesRead = in.read(buffer)) != -1) {
+            out.write(buffer, 0, bytesRead);
         }
     }
 }
